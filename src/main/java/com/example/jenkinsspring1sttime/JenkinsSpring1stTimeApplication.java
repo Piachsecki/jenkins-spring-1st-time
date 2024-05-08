dff632d69947e97ed9e22bd076de0431021da014
Kacper Piasecki <k.r.piasecki2@gmail.com>
2024-05-08 15:47:59 +0200

    adding files

--- src/main/java/com/example/jenkinsspring1sttime/JenkinsSpring1stTimeApplication.java
+++ src/main/java/com/example/jenkinsspring1sttime/JenkinsSpring1stTimeApplication.java
@@ -2,12 +2,21 @@ package com.example.jenkinsspring1sttime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JenkinsSpring1stTimeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JenkinsSpring1stTimeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsSpring1stTimeApplication.class, args);
+    }
+
+
+    @GetMapping
+    public String get(){
+        return "elo";
+    }
 
 }
--- src/test/java/com/example/jenkinsspring1sttime/JenkinsSpring1stTimeApplicationTests.java
+++ src/test/java/com/example/jenkinsspring1sttime/JenkinsSpring1stTimeApplicationTests.java
@@ -1,10 +1,33 @@
 package com.example.jenkinsspring1sttime;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.core.AutoConfigureCache;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class JenkinsSpring1stTimeApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
+    void shouldRerturnDeafultMessage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("elo"));
+    }
+
+}
