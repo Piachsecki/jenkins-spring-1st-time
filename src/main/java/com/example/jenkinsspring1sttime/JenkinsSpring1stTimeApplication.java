e2697c77570028078a6efcfe7cc2e9b180f55130
Kacper Piasecki <k.r.piasecki2@gmail.com>
2024-05-08 15:10:16 +0200

    Initial commit

---
+++ src/main/java/com/example/jenkinsspring1sttime/JenkinsSpring1stTimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsspring1sttime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSpring1stTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsSpring1stTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkinsspring1sttime/JenkinsSpring1stTimeApplicationTests.java
@@ -0,0 +1,10 @@
+package com.example.jenkinsspring1sttime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsSpring1stTimeApplicationTests {
+
+    @Test
+    void contextLoads() {
